9f4dd67167bcf6eef5919b3891261cdf5d6264bc
Rodney Puplampu <101155875+puplampuclou@users.noreply.github.com>
2025-09-12 19:04:04 -0500

    Add files via upload

--- src/paymentservice/index.js
+++ src/paymentservice/index.js
@@ -18,9 +18,10 @@
 
 const logger = require('./logger')
 
-if (process.env.DISABLE_PROFILER) {
+if(process.env.DISABLE_PROFILER) {
   logger.info("Profiler disabled.")
-} else {
+}
+else {
   logger.info("Profiler enabled.")
   require('@google-cloud/profiler').start({
     serviceContext: {
@@ -31,35 +32,32 @@ if (process.env.DISABLE_PROFILER) {
 }
 
 
-if (process.env.ENABLE_TRACING == "1") {
+if(process.env.ENABLE_TRACING == "1") {
   logger.info("Tracing enabled.")
-
-  const { resourceFromAttributes } = require('@opentelemetry/resources');
-
-  const { ATTR_SERVICE_NAME }= require('@opentelemetry/semantic-conventions');
-
+  const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+  const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
   const { GrpcInstrumentation } = require('@opentelemetry/instrumentation-grpc');
   const { registerInstrumentations } = require('@opentelemetry/instrumentation');
-  const opentelemetry = require('@opentelemetry/sdk-node');
+  const { OTLPTraceExporter } = require("@opentelemetry/exporter-otlp-grpc");
+  const { Resource } = require('@opentelemetry/resources');
+  const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
-  const { OTLPTraceExporter } = require('@opentelemetry/exporter-otlp-grpc');
-
-  const collectorUrl = process.env.COLLECTOR_SERVICE_ADDR;
-  const traceExporter = new OTLPTraceExporter({url: collectorUrl});
-
-  const sdk = new opentelemetry.NodeSDK({
-    resource: resourceFromAttributes({
-      [ATTR_SERVICE_NAME]: process.env.OTEL_SERVICE_NAME || 'paymentservice',
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: process.env.OTEL_SERVICE_NAME || 'paymentservice',
     }),
-    traceExporter: traceExporter,
   });
+
+  const collectorUrl = process.env.COLLECTOR_SERVICE_ADDR
+
+  provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({url: collectorUrl})));
+  provider.register();
 
   registerInstrumentations({
     instrumentations: [new GrpcInstrumentation()]
   });
-
-  sdk.start()
-} else {
+}
+else {
   logger.info("Tracing disabled.")
 }
 
